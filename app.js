3ad427495bd7bd1c9dbb492788f84b646e8aa57d
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-10-10 15:51:47 -0700

    add basic server config

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
 
 mongoose.set('strictQuery', false);
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(process.env.DEV_DATABASE), { useUnifiedTopology: true, useNewUrlParser: true };
 }
+
+const app = express();
+
+app.use(cors());
+
+app.listen(process.env.PORT, () => 
+    console.log(`Listening on port ${process.env.PORT}`),
+);
