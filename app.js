4730d73b0fe404f4116baf8fa99ac547ccfe9dcd
bertcherry <119577715+bertcherry@users.noreply.github.com>
2023-10-12 19:11:01 -0700

    fix app require express

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
+const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
+console.log(process.env);
 const cors = require('cors');
 const routes = require('./routes');
 
